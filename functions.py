59241cbf358f02c52764b2e7ab5d05022c2841ef
KiRzEa <21521042@gm.uit.edu.vn>
2022-10-08 13:18:13 +0700

    split data rate 7:3

--- functions.py
+++ functions.py
@@ -1,8 +1,20 @@
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+#cp, restecg, slope, ca, thal: multi-class
+def getDummies(raw):
+    cp = pd.get_dummies(raw['cp'],prefix='cp')
+    restecg = pd.get_dummies(raw['restecg'],prefix='restecg')
+    ca = pd.get_dummies(raw['ca'],prefix='ca')
+    slope = pd.get_dummies(raw['slope'],prefix='slope')
+    thal = pd.get_dummies(raw['thal'],prefix='thal')
+    return pd.concat((raw,cp,restecg,ca,slope,thal),axis=1)
+
 def sigmoid(X):
     return 1.0 / (1 + np.exp(-X))
 
-def computeCost(X, y, Theta):
+def computeCost(X, y, Theta, lambda_):
     m = np.size(y)
     z = sigmoid(X @ Theta)
     J = ((np.log(z) @ (-y)) - (np.log(1 - z) @ (1 - y))) / m
@@ -10,12 +22,12 @@ def computeCost(X, y, Theta):
 
 def computeGradient(X, y, Theta, m):
     return (X.T @ (sigmoid(X @ Theta) - y)) / m
-def GradientDescent(X, y, Theta, alpha=0.1, iter=10000):
+def GradientDescent(X, y, Theta, alpha=1, iter=1000, lambda_=1):
     m = X.shape[0]
     J_hist = np.zeros((iter, 2))
     for i in range(iter):
-        Theta -= alpha * computeGradient(X,y,Theta,m)
-        cost = computeCost(X, y, Theta)
+        Theta = Theta - alpha * computeGradient(X,y,Theta,m)
+        cost = computeCost(X, y, Theta, lambda_)
         J_hist[i, 0] = i
         J_hist[i, 1] = cost
     return Theta, J_hist
@@ -24,7 +36,7 @@ def normalize(X):
     return (X - np.mean(X,axis=0)) / (np.max(X) - np.min(X)).values
 
 def predict(ypred, threshold=0.5):
-    m = len(ypred)
+    m = ypred.shape[0]
     for i in range(m):
         if ypred[i] < threshold:
             ypred[i] = 0
@@ -32,6 +44,24 @@ def predict(ypred, threshold=0.5):
             ypred[i] = 1
     return ypred
 
-def acc(y_true, y_pred):
+def acc(y, y_pred):
+    y_true = np.array(y)
     correct = np.sum(y_true == y_pred)
     return 1.0 * correct / y_true.shape[0]
+def f1_score(y, y_pred):
+    y_true = np.array(y)
+    m = y_true.shape[0]
+    TP = 0.
+    FP = 0.
+    FN = 0.
+    for i in range(m):
+        if y_true[i] == 1 and y_pred[i] == 1:
+            TP += 1
+        if y_true[i] == 0 and y_pred[i] == 1:
+            FP += 1
+        if y_true[i] == 1 and y_pred[i] == 0:
+            FN += 1
+    precision = TP / (TP + FP)
+    recall = TP / (TP + FN)
+    f1 = (2 * precision * recall) / (precision + recall)
+    return precision, recall, f1
--- main.py
+++ main.py
@@ -1,24 +1,46 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 from functions import *
 #Manipulating Data
 raw = pd.read_csv('heart.csv')
-X = raw.drop(['target'],axis=1)
-y = raw['target']
+raw = getDummies(raw)
+raw.drop(columns = ['cp','restecg','slope','ca','thal'], inplace=True)
+X = raw.drop(['target'], axis=1)
 X[['age', 'trestbps','chol','thalach','oldpeak']] = normalize(X[['age', 'trestbps','chol','thalach','oldpeak']])
-m, n = X.shape
-bias_col = np.ones((m, 1))
-X = np.hstack((bias_col, X))
+y = raw['target']
+#526 True Positive
+#499 True Negative
+id_pos = np.where(y.values.reshape(-1) == 1)[0]
+id_neg = np.where(y.values.reshape(-1) == 0)[0]
+#Split data rate 7:3
+pos_train = X.iloc[id_pos[:368]]
+neg_train = X.iloc[id_neg[:349]]
+pos_test = X.iloc[id_pos[368:]]
+neg_test = X.iloc[id_neg[349:]]
+X_train = np.concatenate((pos_train, neg_train),axis=0)
+X_test = np.concatenate((pos_test, neg_test),axis=0)
+y_train = np.concatenate((y.iloc[id_pos[:368]], y.iloc[id_neg[:349]]),axis=0)
+y_test = np.concatenate((y.iloc[id_pos[368:]], y.iloc[id_neg[349:]]),axis=0)
+#-------------------------------------------------------
+m_train, n_train = X_train.shape
+m_test, n_test = X_test.shape
+bias_col_train = np.ones((m_train, 1))
+bias_col_test = np.ones((m_test, 1))
+X_train = np.hstack((bias_col_train, X_train))
+X_test = np.hstack((bias_col_test, X_test))
 #Init parameters
-Theta = np.zeros(n + 1)
+Theta = np.zeros(n_train + 1)
 #Find optimized parameters
-Theta, J_hist = GradientDescent(X[:700, :], y[:700], Theta)
+Theta, J_hist = GradientDescent(X_train, y_train, Theta)
 #Testing
-z = sigmoid(X[700:, :] @ Theta)
+z = sigmoid(X_test @ Theta)
 predicted = predict(z)
-#Accuracy
-print(acc(y[700:],z))
+print(pd.DataFrame(predicted).value_counts())
+#F1_score
+precision, recall, f1 = f1_score(y_test, z)
+print(f"Precision: {precision}\nRecall: {recall}\nF1 score: {f1}")
 #Plot cost function
 plt.figure(1)
 plt.plot(J_hist[:, 0], J_hist[:, 1])
